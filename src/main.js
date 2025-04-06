1f4ed290d8fe6a86398b45a8f9dc7f2da1665408
Yelyzaveta Shevchenko <lizkasheva26@gmail.com>
2025-04-06 19:05:37 +0200

    completed 11 task

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import iziToast from 'izitoast';
+import {
+  createGallery,
+  showLoader,
+  hideLoader,
+  clearGallery,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = input.value.trim();
+
+  showLoader();
+
+  if (query === '') {
+    return;
+  }
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+
+        form.reset();
+        clearGallery();
+        return;
+      }
+      createGallery(data.hits);
+      hideLoader();
+      console.log(data);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'Sorry, something went wrong!',
+        position: 'topRight',
+      });
+      console.log(error);
+    });
+});
